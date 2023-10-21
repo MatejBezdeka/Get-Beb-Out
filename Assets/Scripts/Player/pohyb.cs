c50b7968c63e25a63fb172e6098370aa54a83976
Jan Kopejtko <jan.kopejtko@email.cz>
2023-10-21 15:56:30 +0200

    player test

---
+++ Assets/Scripts/Player/pohyb.cs
@@ -0,0 +1,77 @@
+//Jan Kopejtko, 2022
+
+using UnityEngine;
+
+public class pohyb : MonoBehaviour
+{
+    private CharacterController characterController;
+    [SerializeField] private Animator animator;
+    public Rigidbody rb;
+
+    [SerializeField] private float movementSpeed, rotationSpeed, gravity;
+    private Vector3 movementDirection = Vector3.zero;
+    private bool playerGrounded;
+    private Vector3 inputMovement = Vector3.zero;
+
+    //sounds
+    public AudioSource runningSound;
+    public AudioSource fallingScream;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.visible = false;
+
+
+        runningSound.Play();
+
+    }
+    void Update()
+    {
+        playerGrounded = characterController.isGrounded;
+        //movement
+        if (playerGrounded)
+        {
+            inputMovement = transform.forward * movementSpeed * Input.GetAxisRaw("Vertical");
+        }
+        characterController.Move(inputMovement * Time.deltaTime);
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Horizontal") * rotationSpeed);
+
+
+        //move one frame
+        movementDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(movementDirection * Time.deltaTime);
+
+        //animations
+        animator.SetBool("isRunning", Input.GetAxisRaw("Vertical") > 0);
+        animator.SetBool("isBackRunning", Input.GetAxisRaw("Vertical") < 0);
+        animator.SetBool("isIdle", Input.GetAxisRaw("Vertical") == 0);
+
+        //sound
+        if (!animator.GetBool("isRunning") && runningSound.isPlaying == true)
+        {
+            runningSound.Stop();
+        }
+        else if (animator.GetBool("isRunning") && runningSound.isPlaying == false)
+        {
+            runningSound.Play();
+        }
+
+        //player died
+        if ((int)rb.position.y == -15f)
+        {
+            if (!fallingScream.isPlaying)
+            {
+                fallingScream.Play();
+            }
+        }
+        if (rb.position.y <= -15f)
+        {
+            if (!fallingScream.isPlaying)
+            {
+                //FindObjectOfType<event_manager>().EndGame();
+            }
+        }
+    }
+}
+//todo comment code
