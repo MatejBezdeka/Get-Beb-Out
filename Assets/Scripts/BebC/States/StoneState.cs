bd0330804d7bdf409c3be822d1e02ca649930757
F-Marias <worldlaw159.mar@seznam.cz>
2023-10-22 12:55:48 +0200

    Cursors and StoneState

---
+++ Assets/Scripts/BebC/States/StoneState.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+namespace Beb.States
+{
+    public class StoneState : BebStatesController
+    {
+        private Vector3 destination;
+        private bool phaseOne = true;
+        private GameObject crystal;
+        private float currentMineTime = 0;
+        public StoneState(BebControll beb, pohyb player, Vector3 destination, GameObject crystal) : base(beb, player)
+        {
+            currentState = States.mine;
+            this.destination = destination;
+            this.crystal = crystal;
+        }
+        protected override void Enter()
+        {
+            beb.agent.isStopped = false;
+            beb.agent.SetDestination(destination);
+            base.Enter();
+        }
+
+        protected override void Update()
+        {
+            if (phaseOne)
+            {
+                if (beb.agent.remainingDistance < 0.5f)
+                {
+                    phaseOne = false;
+                }
+            }
+            else
+            {
+                currentMineTime += Time.deltaTime;
+                if (currentMineTime >= beb.mineTime)
+                {
+                    Object.Destroy(crystal);
+                }
+            }
+        }
+
+        IEnumerator Mine()
+        {
+            WaitForSeconds waitForSeconds = new WaitForSeconds(5);
+            yield return waitForSeconds;
+        }
+        protected override void Exit()
+        {
+            base.Exit();
+        }
+    }
+}
